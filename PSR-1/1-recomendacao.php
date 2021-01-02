b63341e02ece83d510738a64fb4c1670f0c2dc81
Gleisson Neves <53718407+gdk46@users.noreply.github.com>
2021-01-02 17:39:58 -0300

    guia de estilo de código

---
+++ PSR-1/1-recomendacao.php
@@ -0,0 +1,18 @@
+<?php
+
+// Padrão de codificação básico
+
+/**
+ * ************************
+ *        OVERVIEW
+ * ************************
+ * 
+ * Os arquivos DEVEM usar apenas tags <?phpe <?=
+ * Os arquivos DEVEM usar apenas UTF-8 sem BOM para código PHP
+ * Os arquivos DEVEM declarar símbolos (classes, funções, constantes, etc.) ou causar efeitos colaterais (por exemplo, gerar saída, alterar configurações .ini, etc.), mas NÃO DEVEM fazer ambos.
+ * Os namespaces e classes DEVEM seguir um PSR de “carregamento automático”: [ PSR-0 , PSR-4 ].
+ * Os nomes das classes DEVEM ser declarados em StudlyCaps
+ * As constantes de classe DEVEM ser declaradas em letras maiúsculas com separadores de sublinhado.
+ * Os nomes dos métodos DEVEM ser declarados em camelCase
+ * 
+ */
